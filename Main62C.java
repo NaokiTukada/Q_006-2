8b5645c2ff6b35b6ed54d122a5beee6578f47837
NaokuTukada <nao3house7@gmail.com>
2025-04-25 14:34:28 +0900

    6-2

--- Main62C.java
+++ Main62C.java
@@ -1,5 +1,42 @@
 package arugo62;
 
-public class Main62C {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class Main62C {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        String[] Line = s.split("");
+        int x = 0;
+        int y = 0;
+        Set<String> point = new HashSet<>();
+        point.add("0,0");  
+
+        for(int i = 0; i < n; i++){
+            switch (Line[i]) {
+                case "R": x += 1; break;
+                case "L": x -= 1; break;
+                case "U": y += 1; break;
+                case "D": y -= 1; break;
+            }
+
+            String current = x + "," + y;
+            if (point.contains(current)) {
+                System.out.println("Yes");
+                sc.close();
+                return;
+            }
+            point.add(current);
+        }
+
+        System.out.println("No");
+        sc.close();
+    }
 }
+
+
+
