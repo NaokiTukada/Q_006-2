9692fb675a6c3bfa5473352c5160c68e43ea6bea
NaokiTukada <nao3house7@gmail.com>
2025-04-25 14:34:58 +0900

    Update Main62C.java

--- Main62C.java
+++ Main62C.java
@@ -1,10 +1,8 @@
-package arugo62;
-
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Main62C {
+public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
